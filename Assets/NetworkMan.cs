7dec48c0bf91e8fdb6749ba9d2320d3caba1e9ae
Ding Zhang <36141388+Dgit35265@users.noreply.github.com>
2020-01-23 23:56:35 -0500

    Lab2 Client Implement

--- Assets/NetworkMan.cs
+++ Assets/NetworkMan.cs
@@ -9,15 +9,25 @@ using System.Net;
 public class NetworkMan : MonoBehaviour
 {
     public UdpClient udp;
+    public GameObject cube;
+    List<GameObject> cubes;
+    Stack<string> newID;
+    Stack<string> dropID;
+    int spawnNum;
+
     // Start is called before the first frame update
     void Start()
     {
+        cubes = new List<GameObject>();
+        newID = new Stack<string>();
+        dropID = new Stack<string>();
+        spawnNum = 0;
+
         udp = new UdpClient();
         
-        udp.Connect("PUT_IP_ADDRESS_HERE",12345);
+        udp.Connect("18.209.27.229", 12345);
 
-        Byte[] sendBytes = Encoding.ASCII.GetBytes("connect");
-      
+        Byte[] sendBytes = Encoding.ASCII.GetBytes("connect");    
         udp.Send(sendBytes, sendBytes.Length);
 
         udp.BeginReceive(new AsyncCallback(OnReceived), udp);
@@ -32,7 +42,9 @@ public class NetworkMan : MonoBehaviour
 
     public enum commands{
         NEW_CLIENT,
-        UPDATE
+        UPDATE,
+        UPDATE_LIST,
+        DROP
     };
     
     [Serializable]
@@ -43,17 +55,18 @@ public class NetworkMan : MonoBehaviour
     [Serializable]
     public class Player{
         public string id;
+        [Serializable]
         public struct receivedColor{
             public float R;
             public float G;
             public float B;
         }
-        public receivedColor color;        
+        public receivedColor color; 
     }
 
     [Serializable]
     public class NewPlayer{
-        
+        public Player[] player;
     }
 
     [Serializable]
@@ -81,9 +94,30 @@ public class NetworkMan : MonoBehaviour
         try{
             switch(latestMessage.cmd){
                 case commands.NEW_CLIENT:
+                    NewPlayer np = JsonUtility.FromJson<NewPlayer>(returnData);
+                    foreach(var it in np.player)
+                    {
+                        newID.Push(it.id);
+                    }
                     break;
                 case commands.UPDATE:
                     lastestGameState = JsonUtility.FromJson<GameState>(returnData);
+                    break;
+                case commands.UPDATE_LIST:
+                    NewPlayer p = JsonUtility.FromJson<NewPlayer>(returnData);
+                    foreach(var it in p.player)
+                    {
+                        newID.Push(it.id);
+                    }
+                    Debug.Log("UPDATE LIST");
+                    break;
+                case commands.DROP:
+                    NewPlayer dp = JsonUtility.FromJson<NewPlayer>(returnData);
+                    foreach(var it in dp.player)
+                    {
+                        dropID.Push(it.id);
+                    }
+                    Debug.Log("DROP CLIENT");
                     break;
                 default:
                     Debug.Log("Error");
@@ -98,16 +132,40 @@ public class NetworkMan : MonoBehaviour
         socket.BeginReceive(new AsyncCallback(OnReceived), socket);
     }
 
-    void SpawnPlayers(){
-
+    void SpawnPlayers(string id){
+        foreach(var it in cubes)
+        {
+            if(it.GetComponent<ID>().id == id)
+                return;
+        }
+        GameObject temp = Instantiate(cube, new Vector3(spawnNum, 0, 0), cube.transform.rotation);
+        temp.GetComponent<ID>().id = id;
+        cubes.Add(temp);
+        spawnNum++;
     }
 
     void UpdatePlayers(){
-
+        foreach(var it in cubes)
+        {
+            foreach(var p in lastestGameState.players)
+            {
+                Color c = new Color(p.color.R, p.color.G, p.color.B);
+                it.GetComponent<Renderer>().material.SetColor("_Color", c);
+            }
+        }
     }
 
-    void DestroyPlayers(){
-
+    void DestroyPlayers(string id){
+        for(int i = 0; i < cubes.Count; i++)
+        {
+            GameObject it = cubes[i];
+            if(it.GetComponent<ID>().id == id)
+            {
+                GameObject temp = it;
+                cubes.Remove(it);
+                Destroy(temp);
+            }
+        }
     }
     
     void HeartBeat(){
@@ -116,8 +174,23 @@ public class NetworkMan : MonoBehaviour
     }
 
     void Update(){
-        SpawnPlayers();
+        if(newID.Count > 0)
+        {
+            for(int i = 0; i < newID.Count; i++)
+            {
+                string it = newID.Pop();
+                SpawnPlayers(it);
+            }
+        }
         UpdatePlayers();
-        DestroyPlayers();
+
+        if(dropID.Count > 0)
+        {
+            for(int i = 0; i < dropID.Count; i++)
+            {
+                string it = dropID.Pop();
+                DestroyPlayers(it);
+            }
+        }
     }
 }

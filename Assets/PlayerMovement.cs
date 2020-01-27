7d0b27ddefa14c4fd6061c8d0f57f23d27e76790
Ding Zhang <36141388+Dgit35265@users.noreply.github.com>
2020-01-27 12:52:04 -0500

    PlayerMovement

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKey("w"))
+        {
+            transform.Translate(new Vector3(3 * Time.deltaTime, 0, 0));
+        }
+        if (Input.GetKey("s"))
+        {
+            transform.Translate(new Vector3(-3 * Time.deltaTime, 0, 0));
+        }
+        if (Input.GetKey("a"))
+        {
+            transform.Translate(new Vector3(0, 0, 3 * Time.deltaTime));
+        }
+        if (Input.GetKey("d"))
+        {
+            transform.Translate(new Vector3(0, 0, -3 * Time.deltaTime));
+        }
+    }
+}
